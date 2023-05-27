25915501de075cdba134554e92d752a39b533b68
Kai.ting.j <dear17530@gmail.com>
2023-05-27 22:12:52 +0800

    0. typescript

---
+++ helloword.js
@@ -0,0 +1,8 @@
+(function () {
+    function sayHi(str) {
+        return '你好' + str;
+    }
+    sayHi('123');
+    var a = 123;
+    console.log(a);
+})();
---
+++ helloword.ts
@@ -0,0 +1,8 @@
+(() => {
+  function sayHi(str: string) {
+    return '你好' + str
+  }
+  sayHi('123')
+  let a: number = 123
+  console.log(a);
+})()
